020f096e9b020caab0c11bb2ed48a9600be92a8b
Nacho <barrojojoaquin2@gmail.com>
2025-06-27 17:20:46 -0300

    Estructura principal para una web de noticias

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TitutloNews from "./components/TituloNews"
+function App() {
+
+  return (
+    <>
+    <TitutloNews></TitutloNews>
+
+    </>
+  )
+
+}
+export default App
---
+++ src/components/TituloNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TituloNews = () => {
+    return (
+        <div>
+            <h1>Titulo</h1>
+        </div>
+    );
+};
+
+export default TituloNews;
